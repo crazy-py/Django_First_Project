fb6c9e4ddfd121776c51d107ce539ebfde32a7d9
Aditya Maurya <aditya.leadzpipe@gmail.com>
2020-02-27 19:12:35 +0530

    Data In List View From DB

--- blog_django/posts/views.py
+++ blog_django/posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 # Create your views here.
 def post_create(request):
     return HttpResponse("<h1>Hello</h1>")
@@ -14,8 +15,10 @@ def post_update(request):
     return HttpResponse("<h1>Updated</h1>")
 
 def post_list(request):
+    query = Posts.objects.all()
     context_data = {
-    'title':'list'
+    'title':'My Posts',
+    'object_list' : query
     }
     return render(request, 'index.html', context_data)
     # return HttpResponse("<h1>List</h1>")
